40ea40da2a03cad20801c7fb3cefd477a2199423
FayOsan <fayosan15@gmail.com>
2021-05-09 18:34:04 -0700

    Unit 5 Lession D Finshed!!

---
+++ Assets/Scripts/DiffcultiyLevel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiffcultiyLevel : MonoBehaviour
+{
+    private Button button;
+    private GameManager gameManager;
+
+    public int diffculty;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(SetDiffculty);
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void SetDiffculty()
+    {
+        Debug.Log(gameObject.name + "was clicked");
+        gameManager.StartGame(diffculty);
+    }
+}
